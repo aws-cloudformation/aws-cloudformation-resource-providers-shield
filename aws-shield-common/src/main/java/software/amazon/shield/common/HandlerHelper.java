07be47ab5c4146086fd4284f5cd7ffa6f610e8ff
Soonil Hong <soonil@amazon.com>
2023-02-28 11:32:06 -0500

    Add common logic to Common module
     - ExceptionConverter
     - HandlerHelper

---
+++ aws-shield-common/src/main/java/software/amazon/shield/common/HandlerHelper.java
@@ -0,0 +1,44 @@
+package software.amazon.shield.common;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import lombok.NonNull;
+import software.amazon.awssdk.services.shield.ShieldClient;
+import software.amazon.awssdk.services.shield.model.ListTagsForResourceRequest;
+import software.amazon.awssdk.services.shield.model.Tag;
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+import software.amazon.cloudformation.resource.IdentifierUtils;
+
+public class HandlerHelper {
+    private static final int MAX_LENGTH_RESOURCE_NAME = 50;
+
+    public static <T> String generateName(@NonNull final ResourceHandlerRequest<T> request) {
+        return IdentifierUtils.generateResourceIdentifier(
+                request.getLogicalResourceIdentifier(),
+                request.getClientRequestToken(),
+                MAX_LENGTH_RESOURCE_NAME
+        );
+    }
+
+    public static <T> List<T> getTags(
+            @NonNull final AmazonWebServicesClientProxy proxy,
+            @NonNull final ShieldClient client,
+            @NonNull final String resourceArn,
+            @NonNull final Function<Tag, T> converter) {
+
+        ListTagsForResourceRequest request =
+                ListTagsForResourceRequest.builder()
+                        .resourceARN(resourceArn)
+                        .build();
+
+        return proxy
+                .injectCredentialsAndInvokeV2(request, client::listTagsForResource)
+                .tags()
+                .stream()
+                .map(converter)
+                .collect(Collectors.toList());
+    }
+}
