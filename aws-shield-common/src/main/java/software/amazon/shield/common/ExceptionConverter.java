475ab82c7c566e5949b0862f2ed9796b40a342e5
Soonil Hong <soonil@amazon.com>
2023-02-28 11:32:06 -0500

    Add common logic to Common module
     - ExceptionConverter
     - HandlerHelper

---
+++ aws-shield-common/src/main/java/software/amazon/shield/common/ExceptionConverter.java
@@ -0,0 +1,48 @@
+package software.amazon.shield.common;
+
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+import software.amazon.awssdk.services.shield.model.AccessDeniedException;
+import software.amazon.awssdk.services.shield.model.AccessDeniedForDependencyException;
+import software.amazon.awssdk.services.shield.model.InternalErrorException;
+import software.amazon.awssdk.services.shield.model.InvalidOperationException;
+import software.amazon.awssdk.services.shield.model.InvalidPaginationTokenException;
+import software.amazon.awssdk.services.shield.model.InvalidParameterException;
+import software.amazon.awssdk.services.shield.model.InvalidResourceException;
+import software.amazon.awssdk.services.shield.model.LimitsExceededException;
+import software.amazon.awssdk.services.shield.model.LockedSubscriptionException;
+import software.amazon.awssdk.services.shield.model.NoAssociatedRoleException;
+import software.amazon.awssdk.services.shield.model.OptimisticLockException;
+import software.amazon.awssdk.services.shield.model.ResourceAlreadyExistsException;
+import software.amazon.awssdk.services.shield.model.ResourceNotFoundException;
+import software.amazon.awssdk.services.shield.model.ShieldException;
+import software.amazon.cloudformation.proxy.HandlerErrorCode;
+
+public class ExceptionConverter<T> {
+
+    private static final Map<Class<? extends ShieldException>, HandlerErrorCode> MAPPING =
+            ImmutableMap.<Class<? extends ShieldException>, HandlerErrorCode>builder()
+                    .put(AccessDeniedException.class, HandlerErrorCode.AccessDenied)
+                    .put(AccessDeniedForDependencyException.class, HandlerErrorCode.AccessDenied)
+                    .put(InternalErrorException.class, HandlerErrorCode.ServiceInternalError)
+                    .put(InvalidOperationException.class, HandlerErrorCode.InvalidRequest)
+                    .put(InvalidPaginationTokenException.class, HandlerErrorCode.InvalidRequest)
+                    .put(InvalidParameterException.class, HandlerErrorCode.InvalidRequest)
+                    .put(InvalidResourceException.class, HandlerErrorCode.InvalidRequest)
+                    .put(LimitsExceededException.class, HandlerErrorCode.ServiceLimitExceeded)
+                    .put(LockedSubscriptionException.class, HandlerErrorCode.ResourceConflict)
+                    .put(NoAssociatedRoleException.class, HandlerErrorCode.InvalidCredentials)
+                    .put(OptimisticLockException.class, HandlerErrorCode.ResourceConflict)
+                    .put(ResourceAlreadyExistsException.class, HandlerErrorCode.AlreadyExists)
+                    .put(ResourceNotFoundException.class, HandlerErrorCode.NotFound)
+                    .build();
+
+    public static HandlerErrorCode convertToErrorCode(RuntimeException error) {
+        HandlerErrorCode translatedErrorCode = MAPPING.get(error.getClass());
+
+        return translatedErrorCode == null
+                ? HandlerErrorCode.GeneralServiceException
+                : translatedErrorCode;
+    }
+}
