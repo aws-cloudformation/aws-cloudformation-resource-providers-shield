9f85528a5472a45d4d9f5c479a10d3ad088e4be5
Maosen Wang <wangmaos@amazon.com>
2023-02-28 11:48:19 -0500

    initialize AWS::Shield::DRTAccess and AWS::Shield::ProactiveEngagement

---
+++ aws-shield-proactiveengagement/src/main/java/software/amazon/shield/proactiveengagement/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.shield.proactiveengagement;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-shield-proactiveengagement.json");
+    }
+}
