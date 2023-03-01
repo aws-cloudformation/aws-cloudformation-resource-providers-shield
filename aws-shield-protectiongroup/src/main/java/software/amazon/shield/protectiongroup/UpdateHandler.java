cf1e2019ff221eb483f180a4f590c46209ea56a1
Soonil Hong <soonil@amazon.com>
2023-02-28 20:31:31 -0500

    Implement CreateHandler for protection

--- aws-shield-protectiongroup/src/main/java/software/amazon/shield/protectiongroup/UpdateHandler.java
+++ aws-shield-protectiongroup/src/main/java/software/amazon/shield/protectiongroup/UpdateHandler.java
@@ -1,27 +1,56 @@
 package software.amazon.shield.protectiongroup;
 
+import lombok.RequiredArgsConstructor;
+import software.amazon.awssdk.services.shield.ShieldClient;
+import software.amazon.awssdk.services.shield.model.UpdateProtectionGroupRequest;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.Logger;
 import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.OperationStatus;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+import software.amazon.shield.common.CustomerAPIClientBuilder;
+import software.amazon.shield.common.ExceptionConverter;
 
+@RequiredArgsConstructor
 public class UpdateHandler extends BaseHandler<CallbackContext> {
+
+    private final ShieldClient client;
+
+    public UpdateHandler() {
+        this.client = CustomerAPIClientBuilder.getClient();
+    }
 
     @Override
     public ProgressEvent<ResourceModel, CallbackContext> handleRequest(
-        final AmazonWebServicesClientProxy proxy,
-        final ResourceHandlerRequest<ResourceModel> request,
-        final CallbackContext callbackContext,
-        final Logger logger) {
+            final AmazonWebServicesClientProxy proxy,
+            final ResourceHandlerRequest<ResourceModel> request,
+            final CallbackContext callbackContext,
+            final Logger logger) {
 
         final ResourceModel model = request.getDesiredResourceState();
 
-        // TODO : put your code here
+        try {
+            final UpdateProtectionGroupRequest updateProtectionGroupRequest =
+                    UpdateProtectionGroupRequest.builder()
+                            .protectionGroupId(model.getProtectionGroupId())
+                            .aggregation(model.getAggregation())
+                            .members(model.getMembers())
+                            .pattern(model.getPattern())
+                            .resourceType(model.getResourceType())
+                            .build();
 
-        return ProgressEvent.<ResourceModel, CallbackContext>builder()
-            .resourceModel(model)
-            .status(OperationStatus.SUCCESS)
-            .build();
+            proxy.injectCredentialsAndInvokeV2(updateProtectionGroupRequest, this.client::updateProtectionGroup);
+
+            return ProgressEvent.<ResourceModel, CallbackContext>builder()
+                    .status(OperationStatus.SUCCESS)
+                    .build();
+
+        } catch (RuntimeException e) {
+            return ProgressEvent.<ResourceModel, CallbackContext>builder()
+                    .status(OperationStatus.FAILED)
+                    .errorCode(ExceptionConverter.convertToErrorCode(e))
+                    .message(e.getMessage())
+                    .build();
+        }
     }
 }
