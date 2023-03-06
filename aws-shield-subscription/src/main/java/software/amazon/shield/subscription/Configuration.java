6562e1719992a665b5c9541d377b990c6f05d289
Soonil Hong <soonil@amazon.com>
2023-03-06 16:28:11 -0500

    Generate init files for Subscription

---
+++ aws-shield-subscription/src/main/java/software/amazon/shield/subscription/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.shield.subscription;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-shield-subscription.json");
+    }
+}
