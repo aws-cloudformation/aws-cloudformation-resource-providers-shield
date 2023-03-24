8cb00c6d050a66a7cdd97c17aa55c3ac66f5df83
Tao Zhang <taozen@amazon.com>
2023-03-23 17:08:12 -0700

    fix protection group

--- aws-shield-protectiongroup/src/main/java/software/amazon/shield/protectiongroup/CreateHandler.java
+++ aws-shield-protectiongroup/src/main/java/software/amazon/shield/protectiongroup/CreateHandler.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import software.amazon.awssdk.services.shield.ShieldClient;
 import software.amazon.awssdk.services.shield.model.CreateProtectionGroupRequest;
+import software.amazon.awssdk.services.shield.model.CreateProtectionGroupResponse;
+import software.amazon.awssdk.services.shield.model.CreateProtectionRequest;
 import software.amazon.awssdk.services.shield.model.Tag;
 import software.amazon.awssdk.utils.CollectionUtils;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
@@ -26,49 +28,54 @@ public class CreateHandler extends BaseHandler<CallbackContext> {
 
     @Override
     public ProgressEvent<ResourceModel, CallbackContext> handleRequest(
-            final AmazonWebServicesClientProxy proxy,
-            final ResourceHandlerRequest<ResourceModel> request,
-            final CallbackContext callbackContext,
-            final Logger logger) {
+        final AmazonWebServicesClientProxy proxy,
+        final ResourceHandlerRequest<ResourceModel> request,
+        final CallbackContext callbackContext,
+        final Logger logger) {
 
         final ResourceModel model = request.getDesiredResourceState();
+        final String protectionGroupId = model.getProtectionGroupId();
 
         try {
-            final CreateProtectionGroupRequest.Builder createProtectionGroupRequest =
-                    CreateProtectionGroupRequest.builder()
-                            .protectionGroupId(model.getProtectionGroupId())
-                            .aggregation(model.getAggregation())
-                            .members(model.getMembers())
-                            .pattern(model.getPattern())
-                            .resourceType(model.getResourceType());
+            final CreateProtectionGroupRequest.Builder createProtectionGroupRequestBuilder =
+                CreateProtectionGroupRequest.builder()
+                    .protectionGroupId(protectionGroupId)
+                    .aggregation(model.getAggregation())
+                    .pattern(model.getPattern());
 
+            if (model.getPattern().equals("ARBITRARY")) {
+                createProtectionGroupRequestBuilder.members(model.getMembers());
+            } else if (model.getPattern().equals("BY_RESOURCE_TYPE")) {
+                createProtectionGroupRequestBuilder.resourceType(model.getResourceType());
+            }
             if (!CollectionUtils.isNullOrEmpty(model.getTags())) {
-                createProtectionGroupRequest.tags(
-                        model.getTags()
-                                .stream()
-                                .map(tag ->
-                                        Tag.builder()
-                                                .key(tag.getKey())
-                                                .value(tag.getValue())
-                                                .build())
-                                .collect(Collectors.toList())
+                createProtectionGroupRequestBuilder.tags(
+                    model.getTags()
+                        .stream()
+                        .map(tag ->
+                            Tag.builder()
+                                .key(tag.getKey())
+                                .value(tag.getValue())
+                                .build())
+                        .collect(Collectors.toList())
                 );
             }
 
             proxy.injectCredentialsAndInvokeV2(
-                    createProtectionGroupRequest.build(),
-                    this.client::createProtectionGroup);
+                createProtectionGroupRequestBuilder.build(),
+                this.client::createProtectionGroup);
 
-            return ProgressEvent.<ResourceModel, CallbackContext>builder()
-                    .status(OperationStatus.SUCCESS)
-                    .build();
+            model.setProtectionGroupArn(String.format("arn:aws:shield::%s:protection-group/%s",
+                request.getAwsAccountId(),
+                protectionGroupId));
 
+            return new ReadHandler().handleRequest(proxy, request, callbackContext, logger);
         } catch (RuntimeException e) {
             return ProgressEvent.<ResourceModel, CallbackContext>builder()
-                    .status(OperationStatus.FAILED)
-                    .errorCode(ExceptionConverter.convertToErrorCode(e))
-                    .message(e.getMessage())
-                    .build();
+                .status(OperationStatus.FAILED)
+                .errorCode(ExceptionConverter.convertToErrorCode(e))
+                .message(e.getMessage())
+                .build();
         }
     }
 }
