ec7f1b9ea1d228f4a7451f3408e7b0f578c8200a
Tao Zhang <taozen@amazon.com>
2023-03-28 00:16:43 -0700

    update protection resource handler

--- aws-shield-protection/src/main/java/software/amazon/shield/protection/UpdateHandler.java
+++ aws-shield-protection/src/main/java/software/amazon/shield/protection/UpdateHandler.java
@@ -37,69 +37,62 @@ public class UpdateHandler extends BaseHandler<CallbackContext> {
 
     @Override
     public ProgressEvent<ResourceModel, CallbackContext> handleRequest(
-            final AmazonWebServicesClientProxy proxy,
-            final ResourceHandlerRequest<ResourceModel> request,
-            final CallbackContext callbackContext,
-            final Logger logger) {
+        final AmazonWebServicesClientProxy proxy,
+        final ResourceHandlerRequest<ResourceModel> request,
+        final CallbackContext callbackContext,
+        final Logger logger) {
 
         final ResourceModel model = request.getDesiredResourceState();
         final String protectionArn = model.getProtectionArn();
-        logger.log(String.format("ReadHandler: %s", protectionArn));
+        final String protectionId = model.getProtectionId();
+        logger.log(String.format("UpdateHandler: protection arn = %s", protectionArn));
+        logger.log(String.format("UpdateHandler: protection id = %s", protectionId));
 
         try {
-            final Protection protection = getProtection(model, proxy);
+            final Protection protection = proxy
+                .injectCredentialsAndInvokeV2(DescribeProtectionRequest.builder()
+                    .protectionId(model.getProtectionId())
+                    .build(), this.client::describeProtection)
+                .protection();
 
             // 1. associate/disassociate healthChecks
             updateHealthCheckAssociation(model, protection, proxy);
 
             // 2. appLayerAutoResponseConfig
             updateAppLayerAutoResponseConfig(
-                    model.getApplicationLayerAutomaticResponseConfiguration(),
-                    model.getResourceArn(),
-                    proxy);
+                model.getApplicationLayerAutomaticResponseConfiguration(),
+                model.getResourceArn(),
+                proxy);
 
             return readProtection(protectionArn, proxy, logger);
 
         } catch (RuntimeException e) {
             return ProgressEvent.<ResourceModel, CallbackContext>builder()
-                    .status(OperationStatus.FAILED)
-                    .errorCode(ExceptionConverter.convertToErrorCode(e))
-                    .message(e.getMessage())
-                    .build();
+                .status(OperationStatus.FAILED)
+                .errorCode(ExceptionConverter.convertToErrorCode(e))
+                .message(e.getMessage())
+                .build();
         }
     }
 
-    private Protection getProtection(final ResourceModel model, final AmazonWebServicesClientProxy proxy) {
-
-        final DescribeProtectionRequest describeProtectionRequest =
-                DescribeProtectionRequest.builder()
-                        .protectionId(model.getProtectionId())
-                        .resourceArn(model.getResourceArn())
-                        .build();
-
-        return proxy
-                .injectCredentialsAndInvokeV2(describeProtectionRequest, this.client::describeProtection)
-                .protection();
-    }
-
     private void updateHealthCheckAssociation(
-            @NonNull final ResourceModel model,
-            @NonNull final Protection protection,
-            @NonNull final AmazonWebServicesClientProxy proxy) {
+        @NonNull final ResourceModel model,
+        @NonNull final Protection protection,
+        @NonNull final AmazonWebServicesClientProxy proxy) {
 
         final Set<String> healthCheckArnsBefore =
-                protection.healthCheckIds()
-                        .stream()
-                        .map(x -> healthCheckArnTemplate + x)
-                        .collect(Collectors.toSet());
+            protection.healthCheckIds()
+                .stream()
+                .map(x -> healthCheckArnTemplate + x)
+                .collect(Collectors.toSet());
 
         final Set<String> healthCheckArnsAfter = new HashSet<>(model.getHealthCheckArns());
 
         final Set<String> intersection =
-                healthCheckArnsBefore
-                        .stream()
-                        .filter(healthCheckArnsAfter::contains)
-                        .collect(Collectors.toSet());
+            healthCheckArnsBefore
+                .stream()
+                .filter(healthCheckArnsAfter::contains)
+                .collect(Collectors.toSet());
 
         healthCheckArnsBefore.removeAll(intersection);
         healthCheckArnsAfter.removeAll(intersection);
@@ -109,49 +102,49 @@ public class UpdateHandler extends BaseHandler<CallbackContext> {
     }
 
     private void associateHealthChecks(
-            @NonNull final String protectionId,
-            final Set<String> healthCheckArns,
-            final AmazonWebServicesClientProxy proxy) {
+        @NonNull final String protectionId,
+        final Set<String> healthCheckArns,
+        final AmazonWebServicesClientProxy proxy) {
 
         healthCheckArns.forEach(
-                arn -> {
-                    final AssociateHealthCheckRequest request =
-                            AssociateHealthCheckRequest.builder()
-                                    .protectionId(protectionId)
-                                    .healthCheckArn(arn)
-                                    .build();
+            arn -> {
+                final AssociateHealthCheckRequest request =
+                    AssociateHealthCheckRequest.builder()
+                        .protectionId(protectionId)
+                        .healthCheckArn(arn)
+                        .build();
 
-                    proxy.injectCredentialsAndInvokeV2(
-                            request,
-                            this.client::associateHealthCheck);
-                }
+                proxy.injectCredentialsAndInvokeV2(
+                    request,
+                    this.client::associateHealthCheck);
+            }
         );
     }
 
     private void disassociateHealthChecks(
-            @NonNull final String protectionId,
-            final Set<String> healthCheckArns,
-            final AmazonWebServicesClientProxy proxy) {
+        @NonNull final String protectionId,
+        final Set<String> healthCheckArns,
+        final AmazonWebServicesClientProxy proxy) {
 
         healthCheckArns.forEach(
-                arn -> {
-                    final DisassociateHealthCheckRequest request =
-                            DisassociateHealthCheckRequest.builder()
-                                    .protectionId(protectionId)
-                                    .healthCheckArn(arn)
-                                    .build();
+            arn -> {
+                final DisassociateHealthCheckRequest request =
+                    DisassociateHealthCheckRequest.builder()
+                        .protectionId(protectionId)
+                        .healthCheckArn(arn)
+                        .build();
 
-                    proxy.injectCredentialsAndInvokeV2(
-                            request,
-                            this.client::disassociateHealthCheck);
-                }
+                proxy.injectCredentialsAndInvokeV2(
+                    request,
+                    this.client::disassociateHealthCheck);
+            }
         );
     }
 
     private void updateAppLayerAutoResponseConfig(
-            final ApplicationLayerAutomaticResponseConfiguration config,
-            @NonNull final String resourceArn,
-            @NonNull final AmazonWebServicesClientProxy proxy) {
+        final ApplicationLayerAutomaticResponseConfiguration config,
+        @NonNull final String resourceArn,
+        @NonNull final AmazonWebServicesClientProxy proxy) {
 
         if (config.getStatus().equals("ENABLED")) {
             if (config.getAction().getBlock() != null) {
@@ -165,62 +158,62 @@ public class UpdateHandler extends BaseHandler<CallbackContext> {
     }
 
     private void enableAppLayerAutoResponseWithBlockAction(
-            @NonNull final String resourceArn,
-            @NonNull final AmazonWebServicesClientProxy proxy) {
+        @NonNull final String resourceArn,
+        @NonNull final AmazonWebServicesClientProxy proxy) {
 
         proxy.injectCredentialsAndInvokeV2(
-                EnableApplicationLayerAutomaticResponseRequest.builder()
-                        .resourceArn(resourceArn)
-                        .action(
-                                ResponseAction.builder()
-                                        .block(BlockAction.builder().build())
-                                        .build())
-                        .build(),
-                this.client::enableApplicationLayerAutomaticResponse);
+            EnableApplicationLayerAutomaticResponseRequest.builder()
+                .resourceArn(resourceArn)
+                .action(
+                    ResponseAction.builder()
+                        .block(BlockAction.builder().build())
+                        .build())
+                .build(),
+            this.client::enableApplicationLayerAutomaticResponse);
     }
 
     private void enableAppLayerAutoResponseWithCountAction(
-            @NonNull final String resourceArn,
-            @NonNull final AmazonWebServicesClientProxy proxy) {
+        @NonNull final String resourceArn,
+        @NonNull final AmazonWebServicesClientProxy proxy) {
 
         proxy.injectCredentialsAndInvokeV2(
-                EnableApplicationLayerAutomaticResponseRequest.builder()
-                        .resourceArn(resourceArn)
-                        .action(
-                                ResponseAction.builder()
-                                        .count(CountAction.builder().build())
-                                        .build())
-                        .build(),
-                this.client::enableApplicationLayerAutomaticResponse);
+            EnableApplicationLayerAutomaticResponseRequest.builder()
+                .resourceArn(resourceArn)
+                .action(
+                    ResponseAction.builder()
+                        .count(CountAction.builder().build())
+                        .build())
+                .build(),
+            this.client::enableApplicationLayerAutomaticResponse);
     }
 
     private void disableAppLayerAutoResponse(
-            @NonNull final String resourceArn,
-            @NonNull final AmazonWebServicesClientProxy proxy) {
+        @NonNull final String resourceArn,
+        @NonNull final AmazonWebServicesClientProxy proxy) {
 
         proxy.injectCredentialsAndInvokeV2(
-                DisableApplicationLayerAutomaticResponseRequest.builder()
-                        .resourceArn(resourceArn)
-                        .build(),
-                this.client::disableApplicationLayerAutomaticResponse);
+            DisableApplicationLayerAutomaticResponseRequest.builder()
+                .resourceArn(resourceArn)
+                .build(),
+            this.client::disableApplicationLayerAutomaticResponse);
     }
 
     private ProgressEvent<ResourceModel, CallbackContext> readProtection(
-            @NonNull final String protectionArn,
-            @NonNull final AmazonWebServicesClientProxy proxy,
-            @NonNull final Logger logger) {
+        @NonNull final String protectionArn,
+        @NonNull final AmazonWebServicesClientProxy proxy,
+        @NonNull final Logger logger) {
 
         final ResourceModel readResourceModel =
-                ResourceModel.builder()
-                        .protectionArn(protectionArn)
-                        .build();
+            ResourceModel.builder()
+                .protectionArn(protectionArn)
+                .build();
 
         return new ReadHandler(this.client).handleRequest(
-                proxy,
-                ResourceHandlerRequest.<ResourceModel>builder()
-                        .desiredResourceState(readResourceModel)
-                        .build(),
-                null,
-                logger);
+            proxy,
+            ResourceHandlerRequest.<ResourceModel>builder()
+                .desiredResourceState(readResourceModel)
+                .build(),
+            null,
+            logger);
     }
 }
