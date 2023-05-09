4a53010521c54e9f6c8da24fb23755c6487235e9
Tao Zhang <taozen@amazon.com>
2023-05-08 17:22:39 -0700

    config retry policy for OptimisticLockException

--- aws-shield-common/src/main/java/software/amazon/shield/common/CustomerAPIClientBuilder.java
+++ aws-shield-common/src/main/java/software/amazon/shield/common/CustomerAPIClientBuilder.java
@@ -1,5 +1,10 @@
 package software.amazon.shield.common;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.Set;
+
 import com.google.common.collect.ImmutableSet;
 import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.core.exception.RetryableException;
@@ -8,49 +13,59 @@ import software.amazon.awssdk.core.retry.RetryPolicy;
 import software.amazon.awssdk.core.retry.backoff.BackoffStrategy;
 import software.amazon.awssdk.core.retry.backoff.EqualJitterBackoffStrategy;
 import software.amazon.awssdk.core.retry.conditions.OrRetryCondition;
-import software.amazon.awssdk.core.retry.conditions.RetryCondition;
 import software.amazon.awssdk.core.retry.conditions.RetryOnClockSkewCondition;
 import software.amazon.awssdk.core.retry.conditions.RetryOnExceptionsCondition;
 import software.amazon.awssdk.core.retry.conditions.RetryOnStatusCodeCondition;
 import software.amazon.awssdk.core.retry.conditions.RetryOnThrottlingCondition;
 import software.amazon.awssdk.services.shield.ShieldClient;
+import software.amazon.awssdk.services.shield.model.OptimisticLockException;
 import software.amazon.cloudformation.LambdaWrapper;
 
-import java.io.IOException;
-import java.time.Duration;
-
 public class CustomerAPIClientBuilder {
+    public static final Set<Class<? extends Exception>> CFN_RETRYABLE_EXCEPTIONS;
+
+    static {
+        Set<Class<? extends Exception>> retryableExceptions =
+            new HashSet<>(SdkDefaultRetrySetting.RETRYABLE_EXCEPTIONS);
+        retryableExceptions.add(OptimisticLockException.class);
+        CFN_RETRYABLE_EXCEPTIONS = ImmutableSet.copyOf(retryableExceptions);
+    }
+
     private static final BackoffStrategy BACKOFF_THROTTLING_STRATEGY =
-            EqualJitterBackoffStrategy.builder()
-                    .baseDelay(Duration.ofMillis(1500)) //account for jitter so 1st retry is ~1 sec
-                    .maxBackoffTime(SdkDefaultRetrySetting.MAX_BACKOFF)
-                    .build();
+        EqualJitterBackoffStrategy.builder()
+            .baseDelay(Duration.ofMillis(1500)) //account for jitter so 1st retry is ~1 sec
+            .maxBackoffTime(SdkDefaultRetrySetting.MAX_BACKOFF)
+            .build();
 
     private static final RetryPolicy RETRY_POLICY =
-            RetryPolicy.builder()
-                    .numRetries(5) //average delay is ~30 sec if all retries attempted
-                    .retryCondition(RetryCondition.defaultRetryCondition())
-                    .throttlingBackoffStrategy(BACKOFF_THROTTLING_STRATEGY)
-                    .build();
+        RetryPolicy.builder()
+            .numRetries(5) //average delay is ~30 sec if all retries attempted
+            .retryCondition(OrRetryCondition.create(
+                RetryOnStatusCodeCondition.create(SdkDefaultRetrySetting.RETRYABLE_STATUS_CODES),
+                RetryOnExceptionsCondition.create(CFN_RETRYABLE_EXCEPTIONS),
+                RetryOnClockSkewCondition.create(),
+                RetryOnThrottlingCondition.create()))
+            .throttlingBackoffStrategy(BACKOFF_THROTTLING_STRATEGY)
+            .build();
 
     private static final RetryPolicy PUT_LOGGING_CONFIG_RETRY_POLICY =
-            RetryPolicy.builder()
-                    .numRetries(5) //average delay is ~30 sec if all retries attempted
-                    .retryCondition(OrRetryCondition.create(
-                            RetryOnStatusCodeCondition.create(SdkDefaultRetrySetting.RETRYABLE_STATUS_CODES),
-                            RetryOnExceptionsCondition
-                                    .create(ImmutableSet.of(RetryableException.class,
-                                            IOException.class)),
-                            RetryOnClockSkewCondition.create(),
-                            RetryOnThrottlingCondition.create()))
-                    .throttlingBackoffStrategy(BACKOFF_THROTTLING_STRATEGY)
-                    .build();
+        RetryPolicy.builder()
+            .numRetries(5) //average delay is ~30 sec if all retries attempted
+            .retryCondition(OrRetryCondition.create(
+                RetryOnStatusCodeCondition.create(SdkDefaultRetrySetting.RETRYABLE_STATUS_CODES),
+                RetryOnExceptionsCondition
+                    .create(ImmutableSet.of(RetryableException.class,
+                        IOException.class)),
+                RetryOnClockSkewCondition.create(),
+                RetryOnThrottlingCondition.create()))
+            .throttlingBackoffStrategy(BACKOFF_THROTTLING_STRATEGY)
+            .build();
 
     public static ShieldClient getClient() {
         return ShieldClient.builder()
-                .overrideConfiguration(ClientOverrideConfiguration.builder()
-                        .retryPolicy(RETRY_POLICY).build())
-                .httpClient(LambdaWrapper.HTTP_CLIENT)
-                .build();
+            .overrideConfiguration(ClientOverrideConfiguration.builder()
+                .retryPolicy(RETRY_POLICY).build())
+            .httpClient(LambdaWrapper.HTTP_CLIENT)
+            .build();
     }
 }
